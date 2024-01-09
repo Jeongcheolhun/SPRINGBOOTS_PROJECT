1ddcdec5222b227ffb9337c481118ffc5612aaef
JEONGCHEOLHUN <hoon2525@gmail.com>
2024-01-09 14:22:54 +0900

    20240109PM

--- 08 장바구니/src/main/resources/static/js/payment/read.js
+++ 08 장바구니/src/main/resources/static/js/payment/read.js
@@ -3,18 +3,18 @@
 const pay_phone_btn = document.querySelector('.pay_phone_btn');
 pay_phone_btn.addEventListener('click',function(){
 
-    IMP.init("imp87380722");
+    IMP.init("imp27854876");
 
     IMP.request_pay({
         pg: "danal",
         pay_method: "phone",
-        merchant_uid: "test_lr4pgiqk",
+        merchant_uid: "test_lr5vzo7r",
         name: "테스트 결제",
         amount: 100,
         buyer_tel: "010-0000-0000",
-    });
-
+      });
 
 })
+
 
 
